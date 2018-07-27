f7d12d9d6b87654c2e34d924aacc709a3e16cd5c
Riza <Riza.Rizae@itvisiongmbh.com>
2018-07-27 17:04:07 +0200

    same chges

---
+++ onlineshopping/src/main/java/de/tqg/onlineshopping/controller/PageController.java
@@ -0,0 +1,19 @@
+package de.tqg.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value= {"/" , "/home", "/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", "welcome to spring web MVC");
+		return mv;
+	}
+
+}
