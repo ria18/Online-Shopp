4c59e4bd5551bdce9bb7909938ff5ef1382fbdb5
Riza <Riza.Rizae@itvisiongmbh.com>
2018-08-13 17:22:59 +0200

    Completed the interface theme and other issues

--- onlineshopping/src/main/java/de/tqg/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/de/tqg/onlineshopping/controller/PageController.java
@@ -12,8 +12,29 @@ public class PageController {
 	@RequestMapping(value= {"/","/home","/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "welcome to spring web MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("UserClickHome", true);
+		
 		return mv;
+	}
+	
+	// add the request mapping in the PageController 
+	@RequestMapping(value= {"/about"})
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("UserClickAbout", true);
+		
+		return mv;
+	}
+
+	@RequestMapping(value= {"/contact"})
+	public ModelAndView contact() {
+		ModelAndView mvContact = new ModelAndView("page");
+		mvContact.addObject("title", "Contact");
+		mvContact.addObject("UserClickContact", true);
+		
+		return mvContact;
 	}
 
 }
